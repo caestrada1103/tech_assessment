413b845046307a43514777573a28901d37400452
Carlos Estrada <30739662+caestrada1103@users.noreply.github.com>
2026-05-20 17:48:31 -0500

    Feature/implement eslint and prettier tools (#2)
    * chore: add ESLint and Prettier as dev dependencies
    
    * chore: add ESLint configuration for JavaScript and JSON files
    
    * chore: update .gitignore to include Cypress and Visual Studio Code configurations
    
    * chore: update ESLint configuration to use ES2024 as the source type for JavaScript files
    
    * chore: add Prettier configuration file for code formatting
    
    * chore: add .prettierignore to exclude build and coverage artifacts
    
    * chore: add TypeScript reference for Cypress in e2e.js

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+import pluginCypress from 'eslint-plugin-cypress'
+
+export default defineConfig([
+  {
+    files: ['cypress/**/*.js'],
+    extends: [
+      pluginCypress.configs.recommended,
+    ],
+    rules: {
+      'cypress/no-unnecessary-waiting': 'off',
+    },
+  },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "ES2024" } },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+]);
