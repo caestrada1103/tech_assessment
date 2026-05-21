476a4aa21334b83bc4150832f568feda129729bc
Carlos Estrada <caestrada1103@gmail.com>
2026-05-20 21:15:06 -0500

    Enhance Cypress configuration to include setup for grep plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,20 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-  numTestsKeptInMemory: 0,
-  defaultCommandTimeout: 30000,
-  pageLoadTimeout: 100000,
-  responseTimeout: 20000,
-  requestTimeout: 1000,
-  chromeWebSecurity: false,
-  env:{
-  },
-  e2e: {
-    baseUrl: 'https://demoqa.com/',
-  },
+	allowCypressEnv: false,
+	numTestsKeptInMemory: 0,
+	defaultCommandTimeout: 30000,
+	pageLoadTimeout: 100000,
+	responseTimeout: 20000,
+	requestTimeout: 1000,
+	chromeWebSecurity: false,
+	env: {},
+	e2e: {
+    setupNodeEvents(on, config) {
+      const { plugin: cypressGrepPlugin } = require('@cypress/grep/plugin')
+      cypressGrepPlugin(config)
+      return config
+    },
+		baseUrl: 'https://demoqa.com/',
+	},
 });
