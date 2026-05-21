e25a9b0ec9d6a066a592ba04baba5ecea828a7e0
Carlos Estrada <caestrada1103@gmail.com>
2026-05-20 21:19:29 -0500

    Refactor homePages.js to use CommonJS syntax for exports

--- cypress/support/constants/homePages.js
+++ cypress/support/constants/homePages.js
@@ -1,6 +1,8 @@
-export const CATEGORIES_CARD = ['Elements', 'Forms', 'Alerts, Frame & Windows', 'Widgets', 'Interactions', 'Book Store Application'];
-export const URL_CATEGORY_MAP = {
+const CATEGORIES_CARD = ['Elements', 'Forms', 'Alerts, Frame & Windows', 'Widgets', 'Interactions', 'Book Store Application'];
+const URL_CATEGORY_MAP = {
 		'Book Store Application': 'books',
 		'Alerts, Frame & Windows': 'alertsWindows',
 		'Interactions': 'interaction',
 	};
+
+module.exports = { CATEGORIES_CARD, URL_CATEGORY_MAP };
