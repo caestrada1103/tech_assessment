f2d02aade38209a3c517224af63e39c61beb759f
Carlos Estrada <caestrada1103@gmail.com>
2026-05-20 20:54:03 -0500

    Feature: add constants for home page categories and URL mapping

---
+++ support/constants/homePages.js
@@ -0,0 +1,6 @@
+export const CATEGORIES_CARD = ['Elements', 'Forms', 'Alerts, Frame & Windows', 'Widgets', 'Interactions', 'Book Store Application'];
+export const URL_CATEGORY_MAP = {
+		'Book Store Application': 'books',
+		'Alerts, Frame & Windows': 'alertsWindows',
+		'Interactions': 'interaction',
+	};
